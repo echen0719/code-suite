5aa0f8ebe5cf92538da403c0b949b0e458588ab9
Eric Chen <echen0719@gmail.com>
2025-01-26 10:33:52 -0500

    Update ascii_gen.py
    added a fill disk feature

--- Python/ascii_gen.py
+++ Python/ascii_gen.py
@@ -1,12 +1,27 @@
 import random
+import os
+import platform
 
 string = ""
+MB = '0' * 1024 * 1024
+system = platform.system()
 
-length = int(input("How many characters? "))
+length = int(input("How many characters (-1 to fill whole disk with 0s)? "))
 
-for i in range(length): 
-   string += chr(random.randint(0,127))
+if length >= 0:
+      for i in range(length):
+            string += chr(random.randint(32,126))
+            with open("ASCII_String.txt", "w") as text:
+                text.write(string)
 
-print(string)
-with open("ASCII_String.txt", "w") as text:
-    text.write(string)
+if length == -1:
+      if system == 'Windows':
+            username = os.getenv('USERNAME')
+            with open(r"C:\Users\{}\0x1a2b3c4d.dll".format(username), "a") as text:
+                  while True:
+                        text.write(MB)
+      if system == 'Linux':
+            username = os.environ.get("USER")
+            with open(r"/home/{}/.0x1a2b3c4d".format(username), "a") as text:
+                  while True:
+                        text.write(MB)
