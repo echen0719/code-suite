dcf8a0c82ec096613d1ea921664e262f8408a905
Eric Chen <echen0719@gmail.com>
2025-02-02 19:28:09 -0500

    Create Bitcoin Puzzle #67 - 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9.py
    New code to solve puzzle #67. Wish me luck (probably will take years for me).

---
+++ Python/Bitcoin Puzzle #67 - 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9.py
@@ -0,0 +1,37 @@
+from pycoin.symbols.btc import network as solver # https://pypi.org/project/pycoin/
+
+# 1. Generate private key between low, high + 1
+# 2. Use pycoin to create address
+# 3. Check if address is equal to reward wallet
+# 4. Stop loop and save to file called found.txt
+
+def up_search():
+    for i in range(73786976294838206464, 147573952589676412928):
+        private_key = solver.parse.secret_exponent(i).address()
+        print("{}. {}".format(i, private_key))
+
+        if (private_key == "1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9"):
+            with open("found.txt", "a") as file:
+                file.write("{}. {} ({})".format(i, private_key, hex(i)))
+            break
+
+# up_search to start 73786976294838206464 up to 147573952589676412928
+
+def down_search():
+        for i in reversed(range(73786976294838206464, 147573952589676412928)):
+            private_key = solver.parse.secret_exponent(i).address()
+            print("{}. {}".format(i, private_key))
+
+            if (private_key == "1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9"):
+                with open("found.txt", "a") as file:
+                    file.write("{}. {} ({})".format(i, private_key, hex(i)))
+                break
+
+# down_search to start at 147573952589676412928 down to 73786976294838206464
+
+if int(input("Up (1) or Down (2): ")) == 1:
+    up_search()
+else:
+    down_search()
+
+# I'll probably ask my friend to help me with C-family code
