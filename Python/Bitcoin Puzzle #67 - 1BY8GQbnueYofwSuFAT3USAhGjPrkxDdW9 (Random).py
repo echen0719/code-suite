1bf5c2002f672cb37d49398de59d59e0f67970c6
Eric Chen <echen0719@gmail.com>
2025-02-02 19:36:02 -0500

    ._.
    Forgot to test before publishing. Quick fix: imported random and edited a few other things

--- Python/Bitcoin Puzzle #67 - 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 (Random).py
+++ Python/Bitcoin Puzzle #67 - 1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9 (Random).py
@@ -1,3 +1,4 @@
+import random
 from pycoin.symbols.btc import network as solver # https://pypi.org/project/pycoin/
 
 # 1. Generate private key between low, high + 1
@@ -5,22 +6,16 @@ from pycoin.symbols.btc import network as solver # https://pypi.org/project/pyco
 # 3. Check if address is equal to reward wallet
 # 4. Stop loop and save to file called found.txt
 
-def up_search():
-    while True:
-        i = random.randint(73786976294838206464, 147573952589676412928)
-        private_key = solver.parse.secret_exponent(i).address()
-        print("{}. {}".format(i, private_key))
+while True:
+    i = random.randint(73786976294838206464, 147573952589676412928)
+    private_key = solver.parse.secret_exponent(i).address()
+    print("{}. {}".format(i, private_key))
 
-        if (private_key == "1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9"):
-            with open("found.txt", "a") as file:
-                file.write("{}. {} ({})".format(i, private_key, hex(i)))
-            break
+    if (private_key == "1BY8GQbnueYofwSuFAT3USAhGjPrkxDdW9"):
+        with open("found.txt", "a") as file:
+            file.write("{}. {} ({})".format(i, private_key, hex(i)))
+        break
 
 # generates a random int between 73786976294838206464 - 147573952589676412928 and takes the address
-
-if int(input("Up (1) or Down (2): ")) == 1:
-    up_search()
-else:
-    down_search()
 
 # I'll probably ask my friend to help me with C-family code
