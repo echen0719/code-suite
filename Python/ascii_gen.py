d217420db7a325f28627ffc3dff599f47a187118
Eric Chen <echen0719@gmail.com>
2025-02-08 11:41:07 -0500

    Update ascii_gen.py
    I don't think using system built writers are slower than Python but it adds another layer of complexity

--- Python/ascii_gen.py
+++ Python/ascii_gen.py
@@ -1,12 +1,5 @@
-import random
-import os
-import platform
-import shutil
-
 string = ""
-#MB = '0' * 1024 * 1024
-free = shutil.disk_usage("/").free / (1024*1024*1024)
-system = platform.system()
+MB = '0' * 1024 * 1024
 
 length = int(input("How many characters (-1 to fill whole disk with 0s)? "))
 
@@ -17,12 +10,6 @@ if length >= 0:
                 text.write(string)
 
 if length == -1:
-      if system == 'Windows':
-            username = os.getenv('USERNAME')
-            path = (r"C:\Users\{}\0x1a2b3c4d.dll".format(username))
-            os.system(fsutil file createnew path free)
-      if system == 'Linux':
-            username = os.getenv('USERNAME')
-            with open(r"/home/{}/.0x1a2b3c4d".format(username), "a") as text:
-                  while True:
-                        print(free)
+      while True:
+            with open("junker", "a") as text:
+                  text.write(MB)
