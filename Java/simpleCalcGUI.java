2fabd7df895b0b3122ed4d2836e1325163764262
Eric Chen <echen0719@gmail.com>
2025-02-26 11:53:49 -0500

    Update simpleCalcGUI.java
    Added addButtons method to simplify buttons. Moved a few things into main method (I don't need swing() method).

--- Java/simpleCalcGUI.java
+++ Java/simpleCalcGUI.java
@@ -6,7 +6,12 @@ public class simpleCalcGUI {
 	private static JFrame frame = new JFrame("Simple 4 Function Calculator");
 
 	public static void main(String[] args) {
-		swing();
+		frame.setSize(425, 550);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel background = new JPanel();
+		background.setBackground(Color.black);
+		frame.setContentPane(background);
+		frame.setLayout(null);
 		buttons();
 		frame.setVisible(true);
 		/* System.out.println(add(1, 2.1, 3.2, 4.3, 5.4, 6.5, 7.6, 8.7)); // 1 + 2.1 + 3.2 + 4.3 + 5.4 + 6.5 + 7.6 + 8.7 = 38.8
@@ -16,141 +21,34 @@ public class simpleCalcGUI {
 	}
 
 	public static void buttons() {
-		JButton ac = new JButton("AC");
-		ac.setBounds(25, 25, 75, 75);
-		ac.setBackground(new Color(92, 92, 95));
-		ac.setForeground(Color.white);
-		ac.setBorder(new RoundedBorder(10));
-		frame.add(ac);
-
-		JButton percent = new JButton("%");
-		percent.setBounds(125, 25, 75, 75);
-		percent.setBackground(new Color(92, 92, 95));
-		percent.setForeground(Color.white);
-		frame.add(percent);
-
-		JButton square = new JButton("x²");
-		square.setBounds(225, 25, 75, 75);
-		square.setBackground(new Color(92, 92, 95));
-		square.setForeground(Color.white);
-		frame.add(square);
-
-		JButton one = new JButton("1");
-		one.setBounds(25, 325, 75, 75);
-		one.setBackground(new Color(42, 42, 44));
-		one.setForeground(Color.white);
-		frame.add(one);
-
-		JButton two = new JButton("2");
-		two.setBounds(125, 325, 75, 75);
-		two.setBackground(new Color(42, 42, 44));
-		two.setForeground(Color.white);
-		frame.add(two);
-
-		JButton three = new JButton("3");
-		three.setBounds(225, 325, 75, 75);
-		three.setBackground(new Color(42, 42, 44));
-		three.setForeground(Color.white);
-		frame.add(three);
-
-		JButton four = new JButton("4");
-		four.setBounds(25, 225, 75, 75);
-		four.setBackground(new Color(42, 42, 44));
-		four.setForeground(Color.white);
-		frame.add(four);
-
-		JButton five = new JButton("5");
-		five.setBounds(125, 225, 75, 75);
-		five.setBackground(new Color(42, 42, 44));
-		five.setForeground(Color.white);
-		frame.add(five);
-
-		JButton six = new JButton("6");
-		six.setBounds(225, 225, 75, 75);
-		six.setBackground(new Color(42, 42, 44));
-		six.setForeground(Color.white);
-		frame.add(six);
-
-		JButton seven = new JButton("7");
-		seven.setBounds(25, 125, 75, 75);
-		seven.setBackground(new Color(42, 42, 44));
-		seven.setForeground(Color.white);
-		frame.add(seven);
-
-		JButton eight = new JButton("8");
-		eight.setBounds(125, 125, 75, 75);
-		eight.setBackground(new Color(42, 42, 44));
-		eight.setForeground(Color.white);
-		frame.add(eight);
-
-		JButton nine = new JButton("9");
-		nine.setBounds(225, 125, 75, 75);
-		nine.setBackground(new Color(42, 42, 44));
-		nine.setForeground(Color.white);
-		frame.add(nine);
-
-		JButton zero = new JButton("0");
-		zero.setBounds(25, 425, 75, 75);
-		zero.setBackground(new Color(42, 42, 44));
-		zero.setForeground(Color.white);
-		frame.add(zero);
-
-		JButton dot = new JButton(".");
-		dot.setBounds(125, 425, 75, 75);
-		dot.setBackground(new Color(42, 42, 44));
-		dot.setForeground(Color.white);
-		frame.add(dot);
-
-		JButton divide = new JButton("/");
-		divide.setBounds(325, 25, 75, 75);
-		divide.setBackground(new Color(255, 159, 10));
-		divide.setForeground(Color.white);
-		frame.add(divide);
-
-		JButton multiply = new JButton("×");
-		multiply.setBounds(325, 125, 75, 75);
-		multiply.setBackground(new Color(255, 159, 10));
-		multiply.setForeground(Color.white);
-		frame.add(multiply);
-
-		JButton minus = new JButton("-");
-		minus.setBounds(325, 225, 75, 75);
-		minus.setBackground(new Color(255, 159, 10));
-		minus.setForeground(Color.white);
-		frame.add(minus);
-
-		JButton plus = new JButton("+");
-		plus.setBounds(325, 325, 75, 75);
-		plus.setBackground(new Color(255, 159, 10));
-		plus.setForeground(Color.white);
-		frame.add(plus);
-
-		JButton plusminus = new JButton("+/-");
-		plusminus.setBounds(225, 425, 75, 75);
-		plusminus.setBackground(new Color(92, 92, 95));
-		plusminus.setForeground(Color.white);
-		frame.add(plusminus);
-
-		JButton equal = new JButton("=");
-		equal.setBounds(325, 425, 75, 75);
-		equal.setBackground(new Color(255, 159, 10));
-		equal.setForeground(Color.white);
-		frame.add(equal);
-	}
-
-	public static void swing() {
-		frame.setSize(425, 550);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		JPanel panel = new JPanel();
-        panel.setBackground(Color.black);
-        frame.setContentPane(panel);
-
-		frame.setLayout(null);
+		addButtons("AC", 25, 25, 75, 75, new Color(92, 92, 95));
+		addButtons("%", 125, 25, 75, 75, new Color(92, 92, 95));
+		addButtons("x²", 225, 25, 75, 75, new Color(92, 92, 95));
+		addButtons("+/-", 25, 425, 75, 75, new Color(92, 92, 95));
+		addButtons("1", 25, 325, 75, 75, new Color(42, 42, 44));
+		addButtons("2", 125, 325, 75, 75, new Color(42, 42, 44));
+		addButtons("3", 225, 325, 75, 75, new Color(42, 42, 44));
+		addButtons("4", 25, 225, 75, 75, new Color(42, 42, 44));
+		addButtons("5", 125, 225, 75, 75, new Color(42, 42, 44));
+		addButtons("6", 225, 225, 75, 75, new Color(42, 42, 44));
+		addButtons("7", 25, 125, 75, 75, new Color(42, 42, 44));
+		addButtons("8", 125, 125, 75, 75, new Color(42, 42, 44));
+		addButtons("9", 225, 125, 75, 75, new Color(42, 42, 44));
+		addButtons("0", 125, 425, 75, 75, new Color(42, 42, 44));
+		addButtons(".", 225, 425, 75, 75, new Color(42, 42, 44));
+		addButtons("/", 325, 25, 75, 75, new Color(255, 159, 10));
+		addButtons("×", 325, 125, 75, 75, new Color(255, 159, 10));
+		addButtons("-", 325, 225, 75, 75, new Color(255, 159, 10));
+		addButtons("+", 325, 325, 75, 75, new Color(255, 159, 10));
+		addButtons("=", 325, 425, 75, 75, new Color(255, 159, 10));
 	}
 
 	public static void addButtons(String symbol, int posX, int posY, int sizeX, int sizeY, Color color) {
-		s
+		JButton button = new JButton(symbol);
+		button.setBounds(posX, posY, sizeX, sizeY);
+		button.setBackground(color);
+		button.setForeground(Color.white);
+		frame.add(button);
 	}
 
 	public static double add(double ... nums) {
