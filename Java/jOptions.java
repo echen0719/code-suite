29a84e969ab427c385f457b28d7c4ec195eb5ca0
Eric Chen <echen0719@gmail.com>
2025-03-02 14:10:14 -0500

    Update jOptions.java

--- Java/jOptions.java
+++ Java/jOptions.java
@@ -3,6 +3,11 @@ import java.awt.*;
 
 public class jOptions {
     public static void main(String[] args) {
+        String inputPass = "";
+        JOptionPane.showMessageDialog(null, "Admin privileges needed for action", "Warning", JOptionPane.WARNING_MESSAGE);
+        do { inputPass = JOptionPane.showInputDialog("Enter sysadmin password: "); }
+        while (!inputPass.equals("1a2b3c"));
 
+        JOptionPane.showMessageDialog(null, "Your PC has been hacked.", "Fatal Error", JOptionPane.ERROR_MESSAGE);
     }
 }
