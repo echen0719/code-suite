0990a144029166c1eaafe05ed00aaad33b4d65d7
Eric Chen <echen0719@gmail.com>
2025-03-21 10:17:53 -0400

    Create turtle_attempt.py

---
+++ Python/turtle_attempt.py
@@ -0,0 +1,31 @@
+import turtle
+import random
+
+turtle.pensize(5)
+turtle.speed("fastest")
+#turtle.speed("slowest")
+
+#for _ in range(1000):
+    #turtle.forward(50)
+    #turtle.left(89.9)
+
+def axes():
+    turtle.goto(0, 0)
+    turtle.goto(-350, 0)
+    turtle.goto(0, 0)
+    turtle.goto(350, 0)
+    turtle.goto(0, 0)
+    turtle.goto(0, -300)
+    turtle.goto(0,0)
+    turtle.goto(0, 300)
+
+def point(x, y):
+    turtle.goto(0, 0)
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.pendown()
+    turtle.dot(10)
+
+axes()
+point(50, 200)
+turtle.done()
