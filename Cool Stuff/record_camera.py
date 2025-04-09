578c88e9a2e219ac131c7c87b2b7806fda898c86
Eric Chen <echen0719@gmail.com>
2025-04-09 10:40:55 -0400

    Update record_camera.py
    Fixed the error preventing any video to be written by changing video dimensions before recording. Added a test camera function, a take image function, and a get color function.

--- Cool Stuff/record_camera.py
+++ Cool Stuff/record_camera.py
@@ -1,31 +1,82 @@
+import os
+os.environ["OPENCV_LOG_LEVEL"] = "SILENT"
+# stop warnings that fill console
 import cv2
-
-cameraIndex = int(input('Which camera are you using (0 for main, 1 for secondary, and so on)?: '))
-
-cam = cv2.VideoCapture(cameraIndex, cv2.CAP_DSHOW)
-if not cam.isOpened():
-    print("Cannot open webcam")
-    exit
+import platform
 
 # https://docs.opencv.org/4.x/dd/d43/tutorial_py_video_display.html
 
-def exitCV():
-    cam.release()
-    cv2.destroyAllWindows()
+platform = platform.system()
+
+def testCameras():
+    working = "Ports Working: "
+    for port in range(0, 17):
+        cam = createCamera(port)
+        check, frame = cam.read()
+        if check: working += str(port) + " "
+    print(working)
+
+def createCamera(cameraIndex):
+    if platform == 'Linux':
+        return cv2.VideoCapture(cameraIndex)
+        print("Cannot open webcam")
+        exit
+
+    elif platform == 'Windows':
+        return cv2.VideoCapture(cameraIndex, cv2.CAP_DSHOW)
+        print("Cannot open webcam")
+        exit
 
 def recordVideo(fileName, vidFormat, fps, sizeX, sizeY):
+    cameraIndex = int(input('Which camera are you using (0 for main, 1 for secondary, and so on)?: '))
+    cam = createCamera(cameraIndex)
+
+    # cv2.VideoCapture() doesn't work if output and input dimensions aren't equal
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, sizeX)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, sizeY)
+
+    print('Started!') # start confirm
     out = cv2.VideoWriter(fileName, cv2.VideoWriter_fourcc(*vidFormat), fps, (sizeX, sizeY))
     while cam.isOpened():
         success, frame = cam.read()
         if not success: break
-        cv2.imshow('Camera Feed Window', frame)
-        out.write(frame)
+        out.write(frame) # write before view
+        cv2.imshow('Video Feed Window', frame)
         if cv2.waitKey(1) == 27: break # press 'ESC' to exit
 
     out.release()
-    exitCV()
+    cam.release()
+    cv2.destroyAllWindows()
+    print('Finished!') # end confirm
 
-print('Started!')
-#recordVideo('out.mp4', 'XVID', 30.0, 1280, 720)
+def takeImage(fileName, sizeX, sizeY):
+    cameraIndex = int(input('Which camera are you using (0 for main, 1 for secondary, and so on)?: '))
+    cam = createCamera(cameraIndex)
+
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, sizeX)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, sizeY)
+
+    if cam.isOpened():
+        success, frame = cam.read()
+        if not success: return
+        cv2.imwrite(fileName, frame)
+
+def getColorOfPixel(sizeX, sizeY, locX, locY):
+    cameraIndex = int(input('Which camera are you using (0 for main, 1 for secondary, and so on)?: '))
+    cam = createCamera(cameraIndex)
+
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, sizeX)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, sizeY)
+
+    if cam.isOpened():
+        success, frame = cam.read()
+        if not success: return
+        b, g, r = frame[locX, locY]
+        return '#{:02x}{:02x}{:02x}'.format(r, g, b), str(r) + ", " + str(g) + ", " + str(b)
+
+testCameras()
+# recordVideo('out.avi', 'XVID', 30.0, 1280, 720)
 recordVideo('out.avi', 'DIVX', 30.0, 1280, 720)
-print('Finished!')
+# takeImage('out.jpg', 1280, 720)
+# hexa, rgb = getColorOfPixel(1280, 720, 300, 300)
+# print(hexa + " or " + rgb + " @ (300, 300)")
