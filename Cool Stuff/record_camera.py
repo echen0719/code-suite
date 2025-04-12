b245e74fc52e909df4da2e1b220b8200040dfcfd
Eric Chen <echen0719@gmail.com>
2025-04-12 19:11:39 -0400

    Update record_camera.py
    Added a function to recordVideo so it will tell the RGB and hex value when recording. This might help in the future.

--- Cool Stuff/record_camera.py
+++ Cool Stuff/record_camera.py
@@ -1,5 +1,5 @@
 import os
-os.environ["OPENCV_LOG_LEVEL"] = "SILENT"
+os.environ['OPENCV_LOG_LEVEL'] = 'SILENT'
 # stop warnings that fill console
 import cv2
 import time
@@ -10,23 +10,30 @@ import platform
 platform = platform.system()
 
 def testCameras():
-    working = "Ports Working: "
+    working = 'Ports Working: '
     for port in range(0, 17):
         cam = createCamera(port)
         check, frame = cam.read()
-        if check: working += str(port) + " "
+        if check: working += str(port) + ' '
     print(working)
 
 def createCamera(cameraIndex):
     if platform == 'Linux':
         return cv2.VideoCapture(cameraIndex)
-        print("Cannot open webcam")
+        print('Cannot open webcam')
         exit
 
     elif platform == 'Windows':
         return cv2.VideoCapture(cameraIndex, cv2.CAP_DSHOW)
-        print("Cannot open webcam")
+        print('Cannot open webcam')
         exit
+
+def getColorOnMouseClick(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        b, g, r = param[y, x]
+        print('RGB color at ({}, {}): ({}, {}, {})'.format(x, y, r, g, b))
+        print('Hex color at ({}, {}): {:02x}{:02x}{:02x}'.format(x, y, r, g, b))
+        # turns out y, x will give correct value, not the other way around
 
 def recordVideo(fileName, vidFormat, fps, sizeX, sizeY):
     cameraIndex = int(input('Which camera are you using (0 for main, 1 for secondary, and so on)?: '))
@@ -39,11 +46,16 @@ def recordVideo(fileName, vidFormat, fps, sizeX, sizeY):
 
     print('Started!') # start confirm
     out = cv2.VideoWriter(fileName, cv2.VideoWriter_fourcc(*vidFormat), fps, (sizeX, sizeY))
+    cv2.namedWindow('Video Feed Window')
+    cv2.setMouseCallback('Video Feed Window', getColorOnMouseClick)
+    
     while cam.isOpened():
         success, frame = cam.read()
         if not success: break
         out.write(frame) # write before view
         cv2.imshow('Video Feed Window', frame)
+        cv2.setMouseCallback('Video Feed Window', getColorOnMouseClick, param=frame)
+        # supposedly, this will cut number of iterations
         if cv2.waitKey(1) == 27: break # press 'ESC' to exit
 
     cam.release()
@@ -69,14 +81,9 @@ def takeImage(sizeX, sizeY, delay, fileName):
     cam.release()
     cv2.destroyAllWindows()
 
-def getColorOfPixel(sizeX, sizeY, locX, locY, delay):
-    frame = takeImage(sizeX, sizeY, delay, "")
-    b, g, r = frame[locX, locY]
-    return '#{:02x}{:02x}{:02x}'.format(r, g, b), str(r) + ", " + str(g) + ", " + str(b)
-
 testCameras()
 # max my webcam can support without losing quality
-# recordVideo('out.avi', 'XVID', 30.0, 640, 480)
-takeImage(640, 480, 3, 'out.jpg')
-hexa, rgb = getColorOfPixel(640, 480, 300, 300, 3)
-print(hexa + " or " + rgb + " @ (300, 300)")
+recordVideo('out.avi', 'XVID', 30.0, 640, 480)
+# takeImage(640, 480, 3, 'out.jpg')
+# hexa, rgb = getColorOfPixel(640, 480, 300, 300, 3)
+# print(hexa + ' or ' + rgb + ' @ (300, 300)')
