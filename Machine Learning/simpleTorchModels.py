a7c2080e2d67034f34ddfd0ec132467f109df04f
Eric Chen <echen0719@gmail.com>
2025-06-01 14:30:28 -0400

    More learning about NN
    Learning more and more...

--- Machine Learning/simpleTorchModels.py
+++ Machine Learning/simpleTorchModels.py
@@ -4,46 +4,97 @@ import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 
-# class to calculate y = mx + b
-class LinearRegModel(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.weights = nn.Parameter(torch.randn(1, dtype=torch.float32))
-        self.bias = nn.Parameter(torch.randn(1, dtype=torch.float32))
+torch.manual_seed(42)
 
-    def forward(self, x):
-        return self.weights * x + self.bias
+def plot(trData, trLabels, teData, teLabels, predLabels=None):
+    # train data
+    plt.scatter(trData, trLabels, c='r', label='Train')
+    # test data
+    plt.scatter(teData, teLabels, c='b', label='Test')
 
-''' Honestly, this would have worked
-def forward(x):
-    weights = nn.Parameter(torch.randn(1, dtype=torch.float32, requires_grad=True))
-    bias = nn.Parameter(torch.randn(1, dtype=torch.float32, requires_grad=True))
-    return weights * x + bias
+    # Y predictions from xTest
+    if predLabels is not None:
+        plt.scatter(teData, predLabels, c='g', label='Predictions')
+
+    plt.legend()
+    plt.show()
+
+'''
+starts with random weights and biases looks at training
+data and adjust these values to be closer to wanted data
+with gradient descent and/or backpropagation
 '''
 
-# matplotlib plotter, nothing special
-def trainAndTestPlotter(xTrain, yTrain, trainLabel, xTest, yTest, testLabel):
-    plt.scatter(xTrain, yTrain, label=trainLabel)
-    plt.scatter(xTest, yTest, label=testLabel)
-    plt.legend(); plt.show()
+# standard model setup
+class linRegModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # require_grad=true (grad meaning gradients)
+        self.weight = nn.Parameter(torch.randn(1, dtype=torch.float))
+        self.bias = nn.Parameter(torch.randn(1, dtype=torch.float))
 
-def linearReg(m=0.75, b=0.25, start=0, end=1, step=0.05):
-    # forms an (x, y) table
-    x = torch.arange(start, end + step, step)
-    y = m * x + b
+    # forward overwritten, x is input tensor
+    def forward(self, x):
+        return self.weight * x + self.bias
+        # actual linear reg
 
-    # 80% data used for training, 20% for testing
-    split = int(0.8 * len(x))
-    xTrain, yTrain = x[:split], y[:split] # 80%
-    xTest, yTest = x[split:], y[split:] # 20%
+# y = mx + b
+weight = 0.7 # m
+bias = 0.3 # b
 
-    # trainAndTestPlotter(xTrain, yTrain, "Train", xTest, yTest, "Test")
+# 0 <= x <= 1
+xTensor = torch.arange(0, 1, 0.02).unsqueeze(dim=1)
+y = weight * xTensor + bias
 
-    torch.manual_seed(42)
-    model1 = LinearRegModel()
+# split into train and test sets
+split = int(0.7 * len(xTensor))
+xTrain, yTrain = xTensor[:split], y[:split]
+xTest, yTest = xTensor[split:], y[split:]
+
+model = linRegModel()
+## print(model.state_dict()) # original
+
+# inference mode turns off gradient
+# with torch.inference_mode(): # .no_grad() also works
+    # yPreds = model(xTest)
+
+# plot(xTrain, yTrain, xTest, yTest, predLabels=yPreds)
+
+# https://docs.pytorch.org/docs/stable/nn.html#loss-functions
+# loss functions - functions to determine inaccuracy
+lossFx = nn.L1Loss()
+
+# https://docs.pytorch.org/docs/stable/optim.html#algorithms
+# optimizer - functions to shift weights and biases
+optim = torch.optim.SGD(params=model.parameters(), lr=0.01)
+# learning rate is the increment parameters change
+
+# training loop
+for trial in range(100):
+    # gradients have to be enabled for backpropagation to work
+    model.train() # all parameters requiring gradients will
+    trPreds = model(xTrain) # forward pass
+    loss = lossFx(trPreds, yTrain) # loss calculation
+    optim.zero_grad() # set back to zero after each loop
+    loss.backward() # backpropagation
+    optim.step() # increments the optimizer
+
+    # training loop
+    model.eval() # put model.eval() inside if validating after each trial
     with torch.inference_mode():
-        yPreds = model1(xTest)
+        tePred = model(xTest) # forward
+        teLoss = lossFx(tePred, yTest) # find loss
 
-    trainAndTestPlotter(xTrain, yTrain, "Train", xTest, yPreds, "Test")
+    if trial % 10 == 0:
+        print("Trial: {} | Loss: {} | Test loss: {}".format(trial, loss, teLoss))
 
-linearReg()
+with torch.inference_mode(): # or else model(xTest) doesn't work'
+    plot(xTrain, yTrain, xTest, yTest, predLabels=model(xTest))
+    # much better results than before
+
+# save the model
+torch.save(obj=model.state_dict(), f='models/simple-linear-reg.pth')
+
+# load the model; must call class
+newModel = linRegModel()
+newModel.load_state_dict(torch.load(f='models/simple-linear-reg.pth'))
