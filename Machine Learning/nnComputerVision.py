8a6e024204022b1d296fcd44847fa8927b86df83
Eric Chen <echen0719@gmail.com>
2025-06-14 17:50:08 -0400

    Update nnComputerVision.py
    Added the training and testing loop to the model. I understand what is happening but this is much more complicated than before.

--- Machine Learning/nnComputerVision.py
+++ Machine Learning/nnComputerVision.py
@@ -9,6 +9,21 @@ from torchvision import transforms
 import matplotlib.pyplot as plt
 import random
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+class CIFARModel(nn.Module):
+    def __init__(self, in_features, out_features, hidden_units=8):
+        super().__init__()
+        self.linearStack = nn.Sequential(
+            nn.Flatten(), # converts to one vector with all data
+            nn.Linear(in_features=in_features, out_features=hidden_units),
+            nn.Linear(in_features=hidden_units, out_features=out_features)
+        )
+    def forward(self, x):
+        return self.linearStack(x)
+
 # use matplotlib to represent tensors as visuals
 def visualization(data):
     images = next(iter(data))[0]
@@ -25,6 +40,7 @@ trainData = datasets.CIFAR10(root="data", train=True, download=True, transform=t
 testData = datasets.CIFAR10(root="data", train=False, download=True, transform=transforms.ToTensor())
 # shape of each tensor is [3, 32, 32]
 
+classesLength = len(trainData.classes) # this multiclass dataset has 10 classes (CIFAR10)
 ## print(trainData.classes) # out: ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 ## print(trainData.class_to_idx); print(trainData[0][1]) # out: 6 which is frog
 ## visualization(trainData)
@@ -33,4 +49,48 @@ testData = datasets.CIFAR10(root="data", train=False, download=True, transform=t
 trainDataLoader = DataLoader(dataset=trainData, batch_size=32, shuffle=True) # 1563 batches
 testDataLoader = DataLoader(dataset=testData, batch_size=32) # 313 batches
 
+# trainFeaturesBatch, trainLabelsBatch = next(iter(trainDataLoader))
+# testFeaturesBatch, testLabelsBatch = next(iter(testDataLoader))
+
 ## visualization(trainDataLoader) # visualizes 9 images from a random train data batch
+
+# 3 color channels * 32px * 32px = 3072 features
+model = CIFARModel(in_features=3072, out_features=classesLength).to(device)
+
+# copied from previous
+def accuracy(yPreds, yTrue):
+    correct = torch.eq(yTrue, yPreds).sum().item()
+    return correct / len(yPreds)
+
+lossFx = nn.CrossEntropyLoss()
+optim = torch.optim.SGD(params=model.parameters(), lr=0.03)
+
+for trial in range(5):
+    accuTrLoss, accuTrAcc = 0, 0
+    # one batch of data goes through inner training loop
+    for batch, (image, label) in enumerate(trainDataLoader):
+        image, label = image.to(device), label.to(device)
+        model.train()
+        trLogits = model(image)
+        trLoss = lossFx(trLogits, label)
+        accuTrLoss += trLoss
+        accuTrAcc += accuracy(label, trLogits.argmax(dim=1))
+        optim.zero_grad()
+        trLoss.backward()
+        optim.step()
+        # model optimizes itself after each batch
+    accuTrLoss /= len(trainDataLoader)
+    accuTrAcc /= len(trainDataLoader)
+
+    accuTeLoss, accuTeAcc = 0, 0
+    model.eval()
+    with torch.inference_mode():
+        for batch, (image, label) in enumerate(testDataLoader):
+            image, label = image.to(device), label.to(device)
+            teLogits = model(image)
+            accuTeLoss += lossFx(teLogits, label)
+            accuTeAcc += accuracy(label, teLogits.argmax(dim=1))
+        accuTeLoss /= len(testDataLoader)
+        accuTeAcc /= len(testDataLoader)
+
+    print("Train {}: TrLoss: {:3f} | TrAcc: {:3f} | TeLoss: {:3f} | TeAcc: {:3f}".format(trial, accuTrLoss, accuTrAcc, accuTeLoss, accuTeAcc))
