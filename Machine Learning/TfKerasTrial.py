e97aca885c48e1e50624021b18de7a67ed77250c
Eric Chen <echen0719@gmail.com>
2025-10-03 19:02:30 -0400

    Update TfKerasTrial.py
    Updated the model a slight bit. Will see what I can do with Fashion MNIST.

--- Machine Learning/TfKerasTrial.py
+++ Machine Learning/TfKerasTrial.py
@@ -38,18 +38,19 @@ print(yTrain.shape)
 print(xTest.shape)
 print(yTest.shape)
 
-model = keras.Sequential(
-    [
-        keras.Input(shape=input_shape),
-        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-        layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-        layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Flatten(),
-        layers.Dropout(0.5),
-        layers.Dense(num_classes, activation="softmax"),
-    ]
-)
+model = keras.Sequential([
+  layers.Conv2D(10, kernel_size=(3, 3), activation="relu"),
+  layers.Conv2D(10, kernel_size=(3, 3), activation="relu"),
+  layers.MaxPooling2D(pool_size=(2, 2)),
+
+  layers.Conv2D(10, kernel_size=(3, 3), activation="relu"),
+  layers.Conv2D(10, kernel_size=(3, 3), activation="relu"),
+  layers.MaxPooling2D(pool_size=(2, 2)),
+
+  layers.Flatten(),
+  layers.Dropout(0.5),
+  layers.Dense(num_classes, activation="softmax")
+])
 
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 model.fit(xTrain, yTrain, batch_size=128, epochs=15)
