b413d7766d5c7194d3e92983d7cc391db8459a6f
Eric Chen <echen0719@gmail.com>
2025-12-23 12:14:05 -0500

    Update linesToArray.py

--- Misc/linesToArray.py
+++ Misc/linesToArray.py
@@ -1,12 +1,15 @@
 print('LinesToArray - This program will take lines of text and create an array for you. To use, following the instruction below.\n')
 print("Write or paste your text. Press Enter (sometimes twice) on an empty line to finish.")
 
+# I tried appends and it didn't seem to work for me so extend is used instead
+# This function will split words on separate lines or separated by spaces
+
 def createArray():
     lines = []
     while True:
         line = input()
-        if not line: break
-        lines.append(line)
+        if not line: break # breaks when user sends a blank line
+        lines.extend(line.split())
     return lines
 
 def returnJavaHashMap(lines):
@@ -16,5 +19,9 @@ def returnJavaHashMap(lines):
         # I will worry about custom format another day
         hashmap += '{}.put("{}", {});\n'.format(variableName, line, 0.0)
     return hashmap
+    
+def returnPythonList(lines):
+    return lines
 
-print("Your array is here:", returnJavaHashMap(createArray()))
+# print("Your array is here:", returnJavaHashMap(createArray()))
+print("Your array is here:", returnPythonList(createArray()))
